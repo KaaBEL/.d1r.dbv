979b2a5f8622ea82cccffc2d7f463dfed8239a97
KaaBEL <som.kaabel@gmail.com>
2024-03-06 00:17:19 +0100

    v.0.1.1 - background is now full Droneboi: conquest background - placing blocks in only by left mouse button - removed old and unused code parts - commands menu in progress - aditional ability to select block type or remove blocks

--- editor.js
+++ editor.js
@@ -1,180 +1,62 @@
 //@ts-check
 "use strict";
-// voxorp-bases.js v0.0.2.9
-
-Number.isInteger = Number.isInteger || function(value) {
-  return typeof value === "number" && 
-    isFinite(value) && 
-    Math.floor(value) === value;
-};
-
-var hiddenLogs = [],
-/** @type {Array<string>} */
-  imgIDs = [],
-/** @type {Array<HTMLImageElement|null>} */
-  images = [];
-/**
- * @typedef {number} integer
- */
-/**
- * @param {OldTile|[string,string,integer,integer,integer]|string} type
- * @param {string} [name]
- * @param {integer} [x]
- * @param {integer} [y]
- * @param {integer|null} [rotation]
- */
-function OldTile(type, name, x, y, rotation) {
-  if (type instanceof OldTile) {
-  /** @type {string} */
-    this.type = type.type;
-  /** @type {string} */
-    this.name = type.name;
-  /** @type {integer} */
-    this.x = type.x;
-  /** @type {integer} */
-    this.y = type.y;
-  /** @type {integer} */
-    this.rotation = type.rotation;
-    return Object.seal(this);
-  }
-  var b = !0;;
-  if ((b = type instanceof Array) && type.length === 5) {
-    name = type[1];
-    x = type[2];
-    y = type[3];
-    rotation = type[4];
-    type = type[0];
-  } else if (arguments.length !== 5 || b)
-    throw new TypeError("OldTile costructor requires 5 arguments:" +
-      " type, x, y, rotation");
-  var n = ["building", "ship"].indexOf(type);
-  if (typeof type != "string")
-    throw new TypeError("1. argument \"type\" is not type of string");
-  if (n < 0)
-    // keys: building, ship
-    throw new TypeError("unknown key value of 1. argument \"type\"");
-  if (typeof name != "string")
-    throw new TypeError("2. argument \"name\" is not type of string");
-  if (typeof x != "number" || !Number.isInteger(x))
-    throw new TypeError("3. argument \"x\" is not an integer");
-  if (typeof y != "number" || !Number.isInteger(y))
-    throw new TypeError("4. argument \"y\" is not an integer");
-  if (n ? !Number.isInteger(rotation) : rotation !== null)
-    throw new TypeError("5. argument \"rotation\" is not " +
-      (n ? "an integer" : "null"));
-  this.type = type;
-  this.name = name;
-  this.x = x;
-  this.y = y;
-  this.rotation = rotation || 0;
-  Object.seal(this);
+// v.0.1.1
+/** @typedef {HTMLElementTagNameMap} N @overload @returns {HTMLDivElement} */
+/** @template {keyof N} K @overload @param {K} e @returns {N[K]} */
+/** @overload @param {string} e @returns {HTMLElement} */
+function EL(e) {
+  return document.createElement(typeof e == "string" ? e : "div");
 }
 
-/** unsealed Typed array!
- * @constructor
- * @param {Array<object|null|Array<any>>} arr
- */
-function RenderedOldObjects(arr) {
-  Object.setPrototypeOf(this, Array.prototype);
-  for (var i = this.length = arr.length; i-- > 0;)
-  /** @type {OldTile} */
-    this[i] = new OldTile(arr[i]);
-  /** @type {function} attaches existing or creates new VoxTile */
-  this.push = function push() {
-    for (var i = arguments.length, args = [], a; i-- > 0;) {
-      a = arguments[i];
-      args[i] = a instanceof OldTile ? a : a ? new OldTile(a) : null;
-    }
-    return [].push.apply(this, args);
-  }
-  this.toString = function toString() {
-    for (var i = 0, s = ""; i < this.length; i++)
-      if (this[i] === null)
-        s += "null; "
-      else
-        s += this[i].name + "," + this[i].x + "," + this[i].y + "; ";
-    return s;
-  }
-}
-/** position which tile has to be inserted in or replaced for
- * @typedef {object} index
- * @property {number} index.i
- * @property {boolean} index.insert
- */
-/** binary search in (where) RenderedOldObjects 
- * @param {RenderedOldObjects} where
- * @param {number} x tile position
- * @param {number} y tile position
- * @returns {index} index to insert or replace tile
-*/
-function binaryFindPos(where, x, y) {
-  var bottom = 0, top = where.length - 1, middle;
-  while (1) {
-    if (bottom === top)
-      break;
-    middle = Math.floor((bottom + top + 1) / 2);
-    if (x > where[middle].x || x === where[middle].x && y > where[middle].y)
-      bottom = middle;
-    else if (y === where[middle].y && x === where[middle].x)
-      return {i: middle, insert: !1};
-    else
-      top = middle - 1;
-  }
-  middle = top + +(x > where[top].x ||
-    x === where[top].x && y > where[top].y);
-  if (middle === where.length)
-    return {i: middle, insert: !1};
-  return {i: middle, insert: (y !== where[top].y || x !== where[top].x)};
-}
-
-var rendered = new RenderedOldObjects([["building", "Wall", 0, 0, null]]);
-
-function placeRandomTile(x, y) {
-  var rand = Math.random() * 5 | 0, index = binaryFindPos(rendered, x, y);
-  var name = "Wall,Scorer,Dynamo empty,Crate,Laser tile".split(",")[rand];
-  var tile = new OldTile("building", name, x, y, null);
-  var i = index.i;
-  if (index.insert)
-    while (i < rendered.length && rendered[i])
-      i++;
-  if (i >= rendered.length)
-    rendered.length++;
-  if (index.insert)
-    for (; i > index.i; i--)
-      rendered[i] = rendered[i - 1];
-  rendered[i] = tile;
-  render();
-}
-// D1R DBV editor v0.1
+/** @typedef addingStyles */
+(function addingStyles(css) {
+  var s = "style", e = GE(s) || document.head.appendChild(EL(s)), t = null;
+  for (e.id = s; t = e.childNodes[0];)
+    e.removeChild(t);
+  e.appendChild(document.createTextNode(css));
+})("#commandsTab" +
+  "{position:fixed;width: 350px;height: 500px;border-radius: 10px;" +
+  "background-color: #000a;color: #888;" +
+  "font-family:monospace,sans-serif,Courier,Consolas;}" +
+  "#commandsTab div:first-child" +
+  "{display: flex;flex-direction: row;border-bottom: 1px solid #777;}" +
+  "#commandsTab div:first-child *{padding: 5px;}" +
+  "#commandsTab button" +
+  "{border: none;background-color: inherit;color: inherit;}" +
+  "#commandsTab div:first-child button" +
+  "{font-weight: bold;padding-top: 3px;}" +
+  "#commandsTab header{flex-grow: 1;}" +
+  "#commandsTab .content button{display: block}"
+);
 
 var imgOverlay = document.body.appendChild(document.createElement("img"));
 imgOverlay.id = "3";
 imgOverlay.style.display = "none";
-imgOverlay.src = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOAAAAAgCAYAAAA\
-LxXRVAAAAAXNSR0IArs4c6QAABShJREFUeF7tXc1O20AQdhyCAmqVQ0+gHsvD9BmQKhAI9RlKSPg\
-pnHiACgGHvkRfppE4VNx6qhRCQxLKbCZZD17vrONduzC5uMbrndmZ+fbbGa/dWhRFk0h+ugVqgc1\
-RCfvHcQzDrtXU8EejUaodlhrLoO9kPEo1k+m+er2u7puo4Y7H48T9zWZYqw8GYeWZpD0ZuRIBUA1\
-zqBgMrEsl7C8ADOz1qTgdgGUHXlXkIyBC6TOVF38PGwLjT7q8vb09OF1fX4fjyclJqjoHBweZap6\
-dnSGDJo6Hh4dwfnt7C8fLy0vKhKHsjfrTia8U+QLAeTihAwSAAsAQcyHEmQDwOQDpDOl7ZgwN+MT\
-4anEdzrsdxWzIUNfX13DebrcTwYjXTRGKDIrXkRG3trYSDPv1VDHlw/AvNvVtZ6qyMCCxSNUcEAo\
-YoeSkBqAAcGaWUuJPGNDMgKGYsFQAYtGpFqsqZTwNQ8z1KONRhqOoNrXHnHI0mlVPy1ryL5oD2op\
-mXCDLEpTJwL4B4rt/02oxIVcAyK5+CwALypBdcwBfQLE5tKDhGruBGRsBSHNByniuOSC2x36wGqo\
-97smsAi8tqWrt8rKbGfp9bG+qLierwIzHTwk/fZx8Tij0o/aNKmhjQmFAJgP6XooKAMHC6UARAM6\
-j1DVQWDOABgJbe7cp0N7alQF9A9G2VITrm5ubcFxbW0ttf35+7joTQ3vc4dLe/wLnlLno87t6XVV\
-P8TccDuGf9Hki7UfLBUkOGO+nDajZnJw+/b3Xe2P3qNZiY+MPnPX76f1G0Rj61X62+IN4ocxHldK\
-YkNWfSxFGAKis7WspagqwhN0FgDwcvlgA/n7/IdMC7379xOusGcBhBuJZnt8qLwOihFBABDndbpc\
-/sn8JzdHRkZMf6E4VzNmQwS4uLqC/RqMBx+FwmPDvysoK6Hl3dwfXHZgwU89mU014vd5bp/HPAWg\
-srrj6vxoMKACcxYEAUIOEAFAZw/sSVAD4bCL2BcTUpSeVXkAuCHK4DGirFjYaDehve3sbVKVMSve\
-apjwXTAxRGHBuDjCsAFAAmLUWFADOHkewUjDnIowA0Bh+RTNhrtyPasfIBYUBldFYgCm9CioAFAA\
-KA5otIDkgv1bmWgXj9rwoE7Jyv9XV1YQ+rVYrVT/bc0HbDhju+3u49MScbnd3NzMH5L4NITmg5IB\
-c4GE7AWAURQJA3pLWOQd0iEbWmlrrz9beQTSrqS8GXBSIheR+rrkglwmvrq6ga/y2C5VjA17evaA\
-3N+o54PSTNVYHaw/iDV8ayLcXtPQc0DryeQMboHwDwKaqb/l5mVAAqHkO94IKAG3h7H7dNwBsGoW\
-SzwVirtyPDtJXLohyPL4NkboXtNVSe0Hv76m7bO5V1weDl7MXlDdifqtQADBpFEq+AFB7IJ+xBBU\
-A8rFTSMtQACgbgNyc0MvSk5sL4tsQ49ED3NLpdOBY1Bvxx8fH0xxOZSba90O5E1QhQad14hp/rhT\
-MSsFcijBlG+B/l28LNAFg0R7O7k8ASOxjmzGKdo+rA4qST4FYSO63QC4It5q+24lMiP275oD4/t/\
-Ozg504boXtCijp/RTlv8TKyJhwLlnQk0AAkDtu6O2zdgCQH8WqMQMpA0vFABpTgjnru/7LeoWbY8\
-odEXf38OdKhpAlJ7T74ea5Nvu4+6sWXR8jPsrEX/CgOEZUAAon6Z/igGYAHQAMiYNafKKLJC6IsA\
-dM6b/HSnjrQLXKuKrMLUA8FW4OdcgBYC5zOZ20yPEnS490BNrKwAAAABJRU5ErkJggg==";
+imgOverlay.src = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOAAAAAgCAYA\
+AAALxXRVAAAAAXNSR0IArs4c6QAABShJREFUeF7tXc1O20AQdhyCAmqVQ0+gHsvD9BmQKhAI9RlK\
+SPgpnHiACgGHvkRfppE4VNx6qhRCQxLKbCZZD17vrONduzC5uMbrndmZ+fbbGa/dWhRFk0h+ugVq\
+gc1RCfvHcQzDrtXU8EejUaodlhrLoO9kPEo1k+m+er2u7puo4Y7H48T9zWZYqw8GYeWZpD0ZuRIB\
+UA1zqBgMrEsl7C8ADOz1qTgdgGUHXlXkIyBC6TOVF38PGwLjT7q8vb09OF1fX4fjyclJqjoHBweZ\
+ap6dnSGDJo6Hh4dwfnt7C8fLy0vKhKHsjfrTia8U+QLAeTihAwSAAsAQcyHEmQDwOQDpDOl7ZgwN\
++MT4anEdzrsdxWzIUNfX13DebrcTwYjXTRGKDIrXkRG3trYSDPv1VDHlw/AvNvVtZ6qyMCCxSNUc\
+EAoYoeSkBqAAcGaWUuJPGNDMgKGYsFQAYtGpFqsqZTwNQ8z1KONRhqOoNrXHnHI0mlVPy1ryL5oD\
+2opmXCDLEpTJwL4B4rt/02oxIVcAyK5+CwALypBdcwBfQLE5tKDhGruBGRsBSHNByniuOSC2x36w\
+Gqo97smsAi8tqWrt8rKbGfp9bG+qLierwIzHTwk/fZx8Tij0o/aNKmhjQmFAJgP6XooKAMHC6UAR\
+AM6j1DVQWDOABgJbe7cp0N7alQF9A9G2VITrm5ubcFxbW0ttf35+7joTQ3vc4dLe/wLnlLno87t6\
+XVVP8TccDuGf9Hki7UfLBUkOGO+nDajZnJw+/b3Xe2P3qNZiY+MPnPX76f1G0Rj61X62+IN4ocxH\
+ldKYkNWfSxFGAKis7WspagqwhN0FgDwcvlgA/n7/IdMC7379xOusGcBhBuJZnt8qLwOihFBABDnd\
+bpc/sn8JzdHRkZMf6E4VzNmQwS4uLqC/RqMBx+FwmPDvysoK6Hl3dwfXHZgwU89mU014vd5bp/HP\
+AWgsrrj6vxoMKACcxYEAUIOEAFAZw/sSVAD4bCL2BcTUpSeVXkAuCHK4DGirFjYaDehve3sbVKVM\
+SveapjwXTAxRGHBuDjCsAFAAmLUWFADOHkewUjDnIowA0Bh+RTNhrtyPasfIBYUBldFYgCm9CioA\
+FAAKA5otIDkgv1bmWgXj9rwoE7Jyv9XV1YQ+rVYrVT/bc0HbDhju+3u49MScbnd3NzMH5L4NITmg\
+5IBc4GE7AWAURQJA3pLWOQd0iEbWmlrrz9beQTSrqS8GXBSIheR+rrkglwmvrq6ga/y2C5VjA17e\
+vaA3N+o54PSTNVYHaw/iDV8ayLcXtPQc0DryeQMboHwDwKaqb/l5mVAAqHkO94IKAG3h7H7dNwBs\
+GoWSzwVirtyPDtJXLohyPL4NkboXtNVSe0Hv76m7bO5V1weDl7MXlDdifqtQADBpFEq+AFB7IJ+x\
+BBUA8rFTSMtQACgbgNyc0MvSk5sL4tsQ49ED3NLpdOBY1Bvxx8fH0xxOZSba90O5E1QhQad14hp/\
+rhTMSsFcijBlG+B/l28LNAFg0R7O7k8ASOxjmzGKdo+rA4qST4FYSO63QC4It5q+24lMiP275oD4\
+/t/Ozg504boXtCijp/RTlv8TKyJhwLlnQk0AAkDtu6O2zdgCQH8WqMQMpA0vFABpTgjnru/7LeoW\
+bY8odEXf38OdKhpAlJ7T74ea5Nvu4+6sWXR8jPsrEX/CgOEZUAAon6Z/igGYAHQAMiYNafKKLJC6\
+IsAdM6b/HSnjrQLXKuKrMLUA8FW4OdcgBYC5zOZ20yPEnS490BNrKwAAAABJRU5ErkJggg==";
 var imgMask = document.body.appendChild(document.createElement("img"));
 imgMask.style.display = "none";
 imgMask.src = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOAAAAAgCAYAAAA\
@@ -190,29 +72,14 @@ i8h+9hum13tnjACAAdnVAnNCHPAAC4BAAAVED0bprqEXRR3k7IG8nY62HH+QG/1lE7wbqpXSNkVb\
 B9l4lAAYDUG6gdwN6FQAg7is5O4C99r/siOTHEKMmYD2I7Z231hJGFQAAAuCXArSgkznwqIPmLHG\
 jDsDsAxgHfCqQ1YJyUwYH3HXAs5yUzBMFrqQA/x3pSrvJWk6nwCc1MChOzix1CgAAAABJRU5ErkJ\
 ggg==";
-/** mask in binary for bit 0 not include texture bit 1 include that pixel */
-var imgIdkActually = base64ToUint8array("///////////////////////////////////////\
-////////////////////////////////////AAAADwAAAA8/////AAAAD/////8AAAAPAAAADwAA\
-AA8AAAAPP////wAAAA//////AAAADwAAAA8AAAAPAAAADw////8AAAAP/////wAAAA8AAAAPAAAA\
-DwAAAA8P////AAAAD/////8AAAAPAAAADwAAAA8AAAAPA////wAAAA//////AAAADwA/wA8AAAAP\
-AAAADwP///8AAAAP/////wAAAA8AP8APAAAADwAAAA8A//////////////8D//wPAP/wDwAAAA8A\
-AAAPAP//////////////A//8DwD/8A8A//APAAAADwA//////////////wP//A8A//APAP/wDwAA\
-AA8AP/////////////8D//wPAP/wDwD/8A8AAAAPAA//////////8P//A//8DwD/8A8A//APAAAA\
-DwAP//////////D//wP//A8A//APAP/wDwAAAA8AA//////////AP/8D//wPAP/wDwD/8A8AAAAP\
-AAP/////////wD//A//8DwD/8A8A//APAAAADwAA/////////8A//wP//A8A//APAP/wDwAAAA8A\
-AP/////////AP/8D//wPAP/wDwD/8A8AAAAPAAA/////////8P//A//8DwD/8A8A//APAAAADwAA\
-P/////////D//wP//A8A//APAP/wDwAAAA8AAA////////////8D//wPAP/wDwD/8A8AAAAPAAAP\
-////////////A//8DwD/8A8AAAAPAAAADwAAA////////////wP//A8A//APAAAADwAAAA8AAAP/\
-//////////8D//wPAP/wDwAAAA8AAAAPAAAA////////////AAAADwD/8A8AAAAPAAAADwAAAP//\
-/////////wAAAA8A//APAAAADwAAAA8AAAA///////////8AAAAPAAAADwAAAA8AAAAPAAAAP///\
-////////AAAADwAAAA8AAAAPAAAADwAAAA///////////wAAAA8AAAAPAAAADwAAAA8AAAAP////\
-//////8AAAAPAAAAD///////////////////////////////////////////////////////////\
-///////////////8=");
-// background attempt 
-var background = document.body.appendChild(document.createElement("img"));
-background.style.display = "none";
-background.src = "https://cdn.cloudflare.steamstatic.com/steam/apps/2112360/\
-library_hero.jpg?t=1702830673"; 
+var dbcBackg = document.body.appendChild(document.createElement("img"));
+dbcBackg.style.display = "none";
+dbcBackg.src = "./assets/_dbc_background.png";
+var helpCanvas = document.createElement("canvas"),
+  rc = function (rc) {
+    return rc instanceof CanvasRenderingContext2D ?
+       rc : new CanvasRenderingContext2D();
+  }(helpCanvas.getContext("2d"));
 
 /**
  * @typedef {[number,number,number]} XYZPosition
@@ -367,41 +234,76 @@ function dbv_findPos() {
     return e.position;
   });
 }
+var placingBlock = function () {
+  return ["Core", "Block", "Wedge", "Small Hydrogen Thruster",
+    "Reaction Wheel","Small Hydrogen Tank", "Small Battery"
+    ][Math.random() * 7 | 0];
+};
 
-/*function placeRect(x0, y0, x1, y1) {
-  var i = 0, ;
-  while (x0 < x1)
-    ;
-}*/
-
-// ! overwrites function press for handling UIs from previous scripts
 press = function press(x, y) {
   x = Math.floor((x - vX - 1) / 2 / sc) * 2;
   y = Math.floor((y - vY - 1) / 2 / sc) * 2;
   for (var i = 0, arr = renderedShip.blocks; i < arr.length; i++)
     if (arr[i].position[0] === y && arr[i].position[2] === -x) {
-      //@ts-ignore
-      arr[i].rotation[2] = arr[i].rotation[2] + 1 & 3;
+      if (placingBlock() === "remove") {
+        arr[i] = arr.slice(-1)[0];
+        arr.length--;
+      } else
+        //@ts-ignore
+        arr[i].rotation[2] = arr[i].rotation[2] + 1 & 3;
       return render();
     }
-  arr.push(new Block(["Core", "Block", "Wedge", "Small Hydrogen Thruster",
-    "Reaction Wheel","Small Hydrogen Tank", "Small Battery"
-    ][Math.random() * 7 | 0],[y, 0, -x], [0, !0, 0]));
+  arr.push(new Block(placingBlock() ,[y, 0, -x], [0, !0, 0]));
   render();
 };
 
-var helpCanvas = document.createElement("canvas"),
-  rc = function (rc) {
-    return rc instanceof CanvasRenderingContext2D ?
-       rc : new CanvasRenderingContext2D();
-  }(helpCanvas.getContext("2d"));
+contextmenu = function commands(x, y) {
+  var id = "commandsTab", nav = GE(id) || EL("nav");
+  nav.style.left = x - 175 + "px";
+  nav.style.top = y - 250 + "px";
+  if (GE(nav.id = id))
+    return;
+  (bd || EL()).appendChild(nav);
+  /** for #commandsTab styles @see {addingStyles} */
+  var e0 = nav.appendChild(EL()),
+  /** @type {HTMLElement} */
+    e1 = e0.appendChild(EL("button"));
+  e1.appendChild(document.createTextNode("\xa0<"));
+  e1 = e0.appendChild(EL("header"));
+  e1.appendChild(document.createTextNode("Hello there is h\u0435cker."));
+  e1 = e0.appendChild(EL("button"));
+  e1.onclick = function () {
+    (bd || EL()).removeChild(nav);
+  };
+  e1.appendChild(document.createTextNode("X\xa0"));
+  (e0 = nav.appendChild(EL())).className = "content";
+  /** @param {string} s */
+  function blockBind(s) {
+    e1 = e0.appendChild(EL("button"));
+    e1.appendChild(document.createTextNode(s));
+    e1.onclick = function () {
+      placingBlock = function () {
+        return s;
+      };
+    };
+  }
+  blockBind("Core");
+  blockBind("Block");
+  blockBind("Wedge");
+  blockBind("Small Hydrogen Thruster");
+  blockBind("Reaction Wheel");
+  blockBind("Small Hydrogen Tank");
+  blockBind("Small Battery");
+  blockBind("remove");
+}
+
 render = function expensiveRenderer() {
   canvas.width = canvas.width;
   try {
-    helpCanvas.width = helpCanvas.height = 620;
-    rc.drawImage(background, -1300, 0);
-    ctx.fillStyle = ctx.createPattern(helpCanvas, "repeat") || "";
-    var n = sc / 8, sx = vX - sc * 37, sy = vY - sc * 37;
+    helpCanvas.width = dbcBackg.naturalWidth || dbcBackg.offsetWidth;
+    helpCanvas.height = dbcBackg.naturalHeight || dbcBackg.offsetHeight;
+    ctx.fillStyle = ctx.createPattern(dbcBackg, "repeat") || "";
+    var n = sc / 2, sx = vX - sc * 37, sy = vY - sc * 37;
     ctx.fillStyle instanceof CanvasPattern &&
       ctx.fillStyle.setTransform(new DOMMatrix([n, 0, 0, n, sx, sy]));
     ctx.fillRect(0, 0, canvas.width, canvas.height);
@@ -430,36 +332,3 @@ render = function expensiveRenderer() {
     ctx.drawImage(helpCanvas, dx, dy, dw, dh);
   }
 };
-
-
-function oldRender() {
-  var i = rendered.length, img, r = rendered;
-  function imageByName(name) {
-    var id = imgIDs.indexOf(name);
-    img = images[id];
-    if (id >= 0 && img !== null)
-      return img;
-    img = new Image();
-    img.src = "./voxorp tiles/" + name + ".svg";
-    img.id = "" + images.length;
-    imgIDs.push(name);
-    images.push(null);
-    img.onload = function setImage() {
-      if (this instanceof HTMLImageElement)
-        images[this.id] = this;
-      oldRender();
-    };
-  }
-
-  canvas.width = canvas.width;
-  while (i-- > 0) {
-  // null in array rendered is empty position
-  // unloaded images returns null, undefined image returns undefined
-    ctx.fillStyle = ["#800", "#080", "#008"][Math.random() * 3 | 0];
-    ctx.fillRect(r[i].x * sc + vX, r[i].y * sc + vY, sc, sc);
-    continue;
-    if (r[i] && imageByName(r[i].name)) {
-      ctx.drawImage(img, r[i].x * sc + vX, r[i].y * sc + vY, sc, sc);
-    }
-  }
-}
