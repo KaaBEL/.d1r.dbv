11bf8db9539e73e3e7a5f9284a3d927ca2c66f14
KaaBEL <som.kaabel@gmail.com>
2024-03-06 00:22:19 +0100

    v.0.0.4 - added contextmenu action bind - enabled old touch UI - HTML now uses semantic Elements

--- defs.ts
+++ defs.ts
@@ -1,4 +1,4 @@
-// v0.0.3 when updated, version inherited from editor.html is used
+// v.0.0.4 when updated, version inherited from editor.html is used
 var _ge: any;
 function GE(v: any) {return document.getElementById(typeof v=="number"&&v==v?
   (_ge=v+1)-1:v==void 0?v=_ge++:v)}
@@ -13,16 +13,15 @@ var sc = 16;
 var canvas: HTMLCanvasElement;
 var ctx: CanvasRenderingContext2D;
 
-function OldTile(tile: OldTile)
-function OldTile(tile: [string, string, integer, integer, integer])
-function OldTile(type: string, name: string, x: integer, y: integer
-  , rotation: integer | null)
-
 var render = function render() {
   return;
 }
 
 var press = function press(x: number, y: number) {
+  return console.log(x, y);
+}
+
+var contextmenu = function contextmenu(x: number, y: number) {
   return console.log(x, y);
 }
 
