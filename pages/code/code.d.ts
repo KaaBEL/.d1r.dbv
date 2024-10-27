17ec72e9c752b833cfd6053247eade375cf76719
KaaBEL <som.kaabel@gmail.com>
2024-10-27 12:30:48 +0100

    v.0.1.64T2 - moved pages related files to `pages/` directory and   adjusted code.d.ts and tsconfig.json to them

--- pages/code/code.d.ts
+++ pages/code/code.d.ts
@@ -1,5 +1,5 @@
-// v.0.1.64T1
-/// <reference path="../editor.html.ts" />
+// v.0.1.64T2
+/// <reference path="./editor.html.ts" />
 /** Definitons of Property item structs for code.js */
 interface Slider {
   min: number;
